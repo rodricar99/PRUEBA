bc58e5f4f442804d3d5232bc0d5d5ec38f647007
rodricar99 <39935861+rodricar99@users.noreply.github.com>
2022-05-24 19:29:32 -0500

    123

--- main.cpp
+++ main.cpp
@@ -1,26 +1,58 @@
-# include <iostream>
-using namespace std ;
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <ctime> 
+using namespace std;
 
-int main (){
-  int n=0,mejor=0;
-  double precio=0,peso=0,posicion=0;
-  cout<<"Ingrese la cantidad de productos: ";
-  cin>>n;
-  cout<<"Ingrese los productos (precio peso)"<<endl;
-  double listaPrecio[n],listaPeso[n],total[n];
-  for(int i=0; i<n;i++){
-    cout<<"Producto "<<i+1<<": ";
-    cin>>precio>>peso;
-    listaPrecio[i] = precio;
-    listaPeso[i] = peso;
-    total[i]=listaPrecio[i]/listaPeso[i];
+int main() {
+  // Generador de numeros aleatorios entre 1 y 1000 //
+
+  srand(time(NULL));
+  int random = rand()%1001, resto = 0, verificador = 0;
+  cout<<random<<endl;
+  unsigned t0, t1;
+  t0=clock();
+
+  // ---------------------------------------------- //
+
+  // Convertidor de numeros a binario //
+
+  string binario = "", llave = " ";
+
+  while(!verificador){
+    resto = random%2;
+    random = random /2;
+    if(random/2==0){
+      binario = to_string(resto)+binario ;
+      if(resto==0)
+        llave = "11"+llave;
+      else
+        llave = "00"+llave;
+      binario = "1" + binario ;
+      llave = "00" + llave;
+      break;
+    }
+    binario = to_string(resto)+binario ;
+    if(resto==0)
+      llave = "11"+llave;
+    else
+      llave = "00"+llave;
+    resto = 0;
   }
-  double menor = total[0];
-  for(int i=0;i<n;i++){
-    if(menor>total[i])
-      menor = total[i];
-      posicion=i;
-  }
-  cout<<"El producto con mejor relacion es el: "<<posicion+1;
+
+  // ---------------------------------------------- //
+
+  // Tiempo de ejecucion de un programa //
+
+  t1 = clock();
+ 
+  double time = (double(t1-t0)/CLOCKS_PER_SEC);
+  cout << "Execution Time: " << time << endl;
+
+  // Claves generadas //
+
+  cout<<binario<<endl;
+  cout<<llave;
+ 
   return 0;
 }
