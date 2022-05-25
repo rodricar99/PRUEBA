ee75803b9defc308dd3e55b7755dffdc51d9845d
rodricar99 <39935861+rodricar99@users.noreply.github.com>
2022-05-24 19:18:15 -0500

    123

--- main.cpp
+++ main.cpp
@@ -1,88 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+
 using namespace std;
 
-class displayDevice{
-	protected:
-		bool activate;
-		string name;
-	public:
-		displayDevice(){
-			activate = false;
-		}
-		void update(){
-			cout<<"se ha modificado el valor"<<endl;
-		}
-		virtual void stateObs() = 0;
-		string getName(){
-			return this->name;
-		}
-};
 
-class forecast:public displayDevice{
-	public:
-		forecast():displayDevice(){
-			name = "ForeCast";
-		}
-		void stateObs() override{
-			activate = true;
-			cout<<"Hello, I am a ForeCast"<<endl;
-		}
-};
 
-class currentCondition:public displayDevice{
-	public:
-		currentCondition():displayDevice(){
-			name = "currentCondition";
-		}
-		void stateObs() override{
-			activate = true;
-			cout<<"Hello, I am a Current Codtition"<<endl;
-		}
-};
+int* generar_arreglo(int cantidad, int valor, int step){
+    int lista[100000];
+    for(int i=0;i<cantidad;i++){
+        if(i%step==0 or i==0)
+            lista[i] = valor;
+        else
+            lista[i] = 0;
+    }
+    return lista;
+}
 
-class statistics:public displayDevice{
-	public:
-		statistics():displayDevice(){
-			name = "statistics";
-		}
-		void stateObs() override{
-			activate = true;
-			cout<<"Hello, I am Statistics"<<endl;
-		}
-};
-
-class weatherStation{
-	private:
-		vector<displayDevice*>* sensors;
-	public:
-		weatherStation(vector<displayDevice*>* _sensors){
-			sensors = _sensors;
-		}
-		void registerObserver(displayDevice* device){
-			sensors->push_back(device);
-		}
-		void removeObserver(displayDevice* device){
-			sensors->erase(std::remove(sensors->begin(),sensors->end(),device),sensors->end());
-		}
-		void notifyObserver(string sensor_){
-			for(int i=0; i<sensors->size(); i++){
-				if((*sensors)[i]->getName() == sensor_){
-					(*sensors)[i]->update();
-				}
-			}	
-		}
-};
-
-int main(){
-	vector<displayDevice*>* device = new vector<displayDevice*>();
-	device->push_back(new forecast());
-	device->push_back(new currentCondition());
-	device->push_back(new statistics());
-	weatherStation* station = new weatherStation(device);
-	station->registerObserver(new forecast());
-	station->registerObserver(new currentCondition());
-	station->notifyObserver("statistics");
-	return 0;
+int main() {
+    int cantidad = 0, valor = 0, step = 0;
+    cin>>cantidad>>valor>>step;
+    int* lista = generar_arreglo(cantidad,valor,step);
+    cout<<"--------------"<<endl;
+    for(int i=0;i<cantidad;i++)
+        cout<<*(lista+i)<<" ";
+    return 0;
 }
